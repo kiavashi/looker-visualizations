7fdd2e7325d0e182578e12d8c71b89494e154b94
Kiavash Page <kiavash@impactsigma.com>
2019-12-13 11:10:30 -0800

    campaigns - test

--- campaigns.js
+++ campaigns.js
@@ -1,3 +1,34 @@
 'use strict';
 
-console.log('campaigns');
+looker.plugins.visualizations.add({
+    create: function(element, config) {
+        element.innerHTML = `
+            <style>
+            .campaigns-table {
+                display: flex;
+                flex-direction: column;
+                background: #ffffff;
+            }
+            .campaign-cell {
+                display: block;
+                background: #f3f3f3;
+                margin-bottom: 10px;
+            }
+            </style>
+            <pre class="campaigns-table"></pre>
+        `;
+    },
+    updateAsync: function(data, element, config, queryResponse, details, done) {
+        let campaignsElement = element.firstElementChild;
+        let html = ''
+        for (let campaign of data) {
+            html += `
+            <div class="campaign-cell">
+                ${JSON.stringify(campaign)}
+            </div>
+            `;
+        }
+
+        campaignsElement.innerHTML = html;
+    }
+})
